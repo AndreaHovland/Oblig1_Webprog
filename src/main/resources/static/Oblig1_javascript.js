2325e8e10391c8c512c0f1ef2420ceed7394222d
AndreaHovland <anhov7918@oslomet.no>
2024-02-18 16:41:46 +0100

    javascript

---
+++ src/main/resources/static/Oblig1_javascript.js
@@ -0,0 +1,38 @@
+let tickets =[];
+
+function submitTicket(){
+    const name = document.getElementById('name').value.trim();
+    const email = document.getElementById('email').value.trim();
+    const phone = document.getElementById('phone').value.trim();
+    const movie = document.getElementById('movie').value.trim();
+
+    if(name === '' || email === '' || phone === '' ||movie ===''){
+        alert('Vennligst fyll ut alle feltene.');
+        return;
+    }
+    //Legg til billetten i arrayet
+    tickets.push({name, email, phone, movie});
+
+    //Poodater visningen
+    displayTickets();
+
+    //Tøm inputfeltene
+    document.getElementById('name').value = '';
+    document.getElementById('email').value = '';
+    document.getElementById('phone').value = '';
+    document.getElementById('movie').value = '';
+
+}
+function deleteAllTickets(){
+    tickets = [];
+    displayTickets();
+}
+function displayTickets(){
+    const ticketList = document.getElementById('ticketList');
+    ticketList.innerHTML = '';
+
+    tickets.forEach(ticket=>{
+        const li = document.createElement('li');
+        li.textContent = `Navn: ${ticket.name}, E-post: ${ticket.email}, Telefon: ${ticket.phone}, Film: ${ticket.movie}`;
+    });
+}
